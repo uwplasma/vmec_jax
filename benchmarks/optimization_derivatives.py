uwplasma/vmex

Add external optimizer and parallel derivative paths

benchmarks/optimization_derivatives.py
@@ -1,0 +1,67 @@
+#!/usr/bin/env python
+"""Compare exact and parallel-FD VMEX derivatives under one physics contract."""
+
+from __future__ import annotations
+
+import argparse
+import json
+import time
+from pathlib import Path
+
+import numpy as np
+
+from vmex import optimize as opt
+from vmex.core.input import VmecInput
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--input",
+        type=Path,
+        default=Path(__file__).resolve().parents[1] / "examples/data/input.solovev",
+    )
+    parser.add_argument("--max-mode", type=int, default=1)
+    parser.add_argument("--workers", type=int)
+    args = parser.parse_args()
+
+    inp = VmecInput.from_file(args.input)
+    terms = [(opt.aspect_ratio, 4.0, 1.0)]
+    exact = opt.VmecProblem.from_tuples(inp, terms, max_mode=args.max_mode)
+    finite = opt.VmecProblem.from_tuples(
+        inp,
+        terms,
+        max_mode=args.max_mode,
+        derivatives="finite_difference",
+        workers=args.workers,
+    )
+
+    # Compile and populate each lane before timing.  Mixing a cold exact JAX
+    # trace with already-compiled FD forward solves is not a fair comparison.
+    exact.residual_jac(exact.x0)
+    finite.residual_jac(finite.x0)
+
+    started = time.perf_counter()
+    exact_jacobian = exact.residual_jac(exact.x0)
+    exact_seconds = time.perf_counter() - started
+    started = time.perf_counter()
+    finite_jacobian = finite.residual_jac(finite.x0)
+    finite_seconds = time.perf_counter() - started
+    error = np.linalg.norm(exact_jacobian - finite_jacobian) / max(
+        np.linalg.norm(exact_jacobian), 1.0
+    )
+    print(json.dumps({
+        "input": str(args.input.resolve()),
+        "max_mode": args.max_mode,
+        "dofs": int(exact.x0.size),
+        "finite_difference_probes": int(2 * exact.x0.size),
+        "cache_state": "warm",
+        "exact_seconds": exact_seconds,
+        "finite_difference_seconds": finite_seconds,
+        "relative_jacobian_error": float(error),
+        "workers": args.workers if args.workers is not None else "auto",
+    }, indent=2, sort_keys=True))
+
+
+if __name__ == "__main__":
+    main()

tests/test_optimize.py
@@ -563,8 +563,13 @@ def test_public_problem_factory_validation():
         opt.make_problem(inp)
     with pytest.raises(ValueError, match="exactly one"):
         opt.make_problem(inp, objective_terms=term, loss=opt.aspect_ratio)
-    with pytest.raises(ValueError, match="currently supports"):
-        opt.make_problem(inp, objective_terms=term, derivatives="finite_difference")
+    finite_difference = opt.make_problem(
+        inp,
+        objective_terms=term,
+        derivatives="finite_difference",
+        workers=1,
+    )
+    assert finite_difference.metadata["derivatives"] == "finite_difference"
     with pytest.raises(ValueError, match="non-negative"):
         opt.make_problem(
             inp, objective_terms=[(opt.aspect_ratio, 4.0, -1.0)], max_mode=1

tests/test_problem.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 import numpy as np
 import pytest
+from types import SimpleNamespace
 
 from vmex.core.problem import Evaluation, FunctionProblem
 
@@ -125,6 +126,29 @@ def test_jax_fallbacks_and_jacobian_shape_validation():
     )
     with pytest.raises(ValueError, match="must have shape"):
         malformed.residual_and_jac(malformed.x0)
+    separate_malformed = FunctionProblem(
+        [1.0, 2.0], residual=lambda x: x, residual_jac=lambda x: np.ones((2, 1))
+    )
+    with pytest.raises(ValueError, match="one column"):
+        separate_malformed.residual_jac(separate_malformed.x0)
+
+
+def test_separate_residual_does_not_eagerly_compute_jacobian():
+    calls = {"residual": 0, "jacobian": 0}
+
+    def residual(x):
+        calls["residual"] += 1
+        return x - 1.0
+
+    def jacobian(x):
+        calls["jacobian"] += 1
+        return np.eye(x.size)
+
+    problem = FunctionProblem([2.0, 3.0], residual=residual, residual_jac=jacobian)
+    np.testing.assert_array_equal(problem.residual(problem.x0), [1.0, 2.0])
+    assert calls == {"residual": 1, "jacobian": 0}
+    np.testing.assert_array_equal(problem.residual_jac(problem.x0), np.eye(2))
+    assert calls == {"residual": 1, "jacobian": 1}
 
 
 def test_problem_rejects_ambiguous_metadata_shapes():
@@ -184,3 +208,127 @@ def test_direct_jaxopt_and_optax_contracts():
         updates, state = transform.update(grad, state, x)
         x = optax.apply_updates(x, updates)
     assert float(problem.jax_fun(x)) < float(problem.jax_fun(problem.x0))
+
+
+def test_vmec_finite_difference_factory_uses_parallel_provider(monkeypatch):
+    """The VMEC factory composes tuples and differentiates opaque host terms."""
+    from pathlib import Path
+
+    from vmex.core import optimize as opt
+    from vmex.core.input import VmecInput
+
+    inp = VmecInput.from_file(
+        Path(__file__).resolve().parents[1] / "examples/data/input.solovev"
+    )
+
+    def fake_solve(trial, **kwargs):
+        del kwargs
+        return SimpleNamespace(value=np.sum(opt.pack_boundary(trial, 1)))
+
+    monkeypatch.setattr(opt, "solve_equilibrium", fake_solve)
+    problem = opt.VmecProblem.from_tuples(
+        inp,
+        [(lambda equilibrium: equilibrium.value, 0.0, 4.0)],
+        max_mode=1,
+        derivatives="finite_difference",
+        workers=2,
+    )
+    jacobian = problem.residual_jac(problem.x0)
+    np.testing.assert_allclose(jacobian, np.full((1, problem.x0.size), 2.0))
+    assert problem.metadata["fd_method"] == "3-point"
+
+
+def test_vmec_finite_difference_scalar_and_validation_paths(monkeypatch):
+    """Opaque scalar losses remain open to any gradient-based optimizer."""
+    from pathlib import Path
+
+    from vmex.core import optimize as opt
+    from vmex.core.input import VmecInput
+
+    inp = VmecInput.from_file(
+        Path(__file__).resolve().parents[1] / "examples/data/input.solovev"
+    )
+
+    def fake_solve(trial, **kwargs):
+        del kwargs
+        return SimpleNamespace(value=float(np.sum(opt.pack_boundary(trial, 1))))
+
+    monkeypatch.setattr(opt, "solve_equilibrium", fake_solve)
+    term = lambda equilibrium: np.atleast_1d(equilibrium.value)  # noqa: E731
+    with pytest.raises(ValueError, match="weight_mode"):
+        opt.make_problem(
+            inp,
+            objective_terms=[(term, 0.0, 1.0)],
+            derivatives="finite_difference",
+            weight_mode="bad",
+        )
+    with pytest.raises(ValueError, match="non-negative"):
+        opt.make_problem(
+            inp,
+            objective_terms=[(term, 0.0, -1.0)],
+            derivatives="finite_difference",
+        )
+    with pytest.raises(FloatingPointError, match="non-finite objective"):
+        opt.make_problem(
+            inp,
+            loss=lambda equilibrium: np.nan,
+            derivatives="finite_difference",
+        )
+    with pytest.raises(FloatingPointError, match="empty residual"):
+        opt.make_problem(
+            inp,
+            objective_terms=[(lambda equilibrium: np.array([]), 0.0, 1.0)],
+            derivatives="finite_difference",
+        )
+
+    problem = opt.make_problem(
+        inp,
+        loss=lambda equilibrium: equilibrium.value**2,
+        derivatives="finite_difference",
+        workers=1,
+        use_ess=False,
+    )
+    value, gradient = problem.value_and_grad(problem.x0)
+    assert np.isfinite(value) and np.all(np.isfinite(gradient))
+
+
+def test_vmec_finite_difference_failed_probe_penalties(monkeypatch):
+    """Failed opaque probes become finite penalties for external optimizers."""
+    from pathlib import Path
+
+    from vmex.core import optimize as opt
+    from vmex.core.input import VmecInput
+
+    inp = VmecInput.from_file(
+        Path(__file__).resolve().parents[1] / "examples/data/input.solovev"
+    )
+    seed = opt.pack_boundary(inp, 1)
+
+    def flaky_solve(trial, **kwargs):
+        del kwargs
+        x = opt.pack_boundary(trial, 1)
+        if not np.array_equal(x, seed):
+            raise RuntimeError("synthetic VMEC failure")
+        return SimpleNamespace(value=float(np.sum(x)))
+
+    monkeypatch.setattr(opt, "solve_equilibrium", flaky_solve)
+    term = lambda equilibrium: np.atleast_1d(equilibrium.value)  # noqa: E731
+    residual_problem = opt.make_problem(
+        inp,
+        objective_terms=[(term, 0.0, 1.0)],
+        derivatives="finite_difference",
+        workers=1,
+    )
+    trial = seed.copy()
+    trial[0] += 0.1
+    assert np.all(np.isfinite(residual_problem.residual(trial)))
+    assert residual_problem.metadata["holder"]["failed_trials"] == 1
+
+    scalar_problem = opt.make_problem(
+        inp,
+        loss=lambda equilibrium: equilibrium.value,
+        derivatives="finite_difference",
+        workers=1,
+    )
+    assert np.isfinite(scalar_problem.fun(trial))
+    assert scalar_problem.metadata["holder"]["failed_trials"] == 1

uwplasma/vmec_jax

benchmarks/optimization_derivatives.py
@@ -1,0 +1,67 @@
+#!/usr/bin/env python
+"""Compare exact and parallel-FD VMEX derivatives under one physics contract."""
+
+from __future__ import annotations
+
+import argparse
+import json
+import time
+from pathlib import Path
+
+import numpy as np
+
+from vmex import optimize as opt
+from vmex.core.input import VmecInput
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--input",
+        type=Path,
+        default=Path(__file__).resolve().parents[1] / "examples/data/input.solovev",
+    )
+    parser.add_argument("--max-mode", type=int, default=1)
+    parser.add_argument("--workers", type=int)
+    args = parser.parse_args()
+
+    inp = VmecInput.from_file(args.input)
+    terms = [(opt.aspect_ratio, 4.0, 1.0)]
+    exact = opt.VmecProblem.from_tuples(inp, terms, max_mode=args.max_mode)
+    finite = opt.VmecProblem.from_tuples(
+        inp,
+        terms,
+        max_mode=args.max_mode,
+        derivative_method="finite_difference",
+        workers=args.workers,
+    )
+
+    # Compile and populate each lane before timing.  Mixing a cold exact JAX
+    # trace with already-compiled FD forward solves is not a fair comparison.
+    exact.residual_jac(exact.x0)
+    finite.residual_jac(finite.x0)
+
+    started = time.perf_counter()
+    exact_jacobian = exact.residual_jac(exact.x0)
+    exact_seconds = time.perf_counter() - started
+    started = time.perf_counter()
+    finite_jacobian = finite.residual_jac(finite.x0)
+    finite_seconds = time.perf_counter() - started
+    error = np.linalg.norm(exact_jacobian - finite_jacobian) / max(
+        np.linalg.norm(exact_jacobian), 1.0
+    )
+    print(json.dumps({
+        "input": str(args.input.resolve()),
+        "max_mode": args.max_mode,
+        "dofs": int(exact.x0.size),
+        "finite_difference_probes": int(2 * exact.x0.size),
+        "cache_state": "warm",
+        "exact_seconds": exact_seconds,
+        "finite_difference_seconds": finite_seconds,
+        "relative_jacobian_error": float(error),
+        "workers": args.workers if args.workers is not None else "auto",
+    }, indent=2, sort_keys=True))
+
+
+if __name__ == "__main__":
+    main()

tests/test_optimize.py
@@ -693,10 +693,16 @@ def test_public_problem_factory_validation():
         opt.make_problem(inp)
     with pytest.raises(ValueError, match="exactly one"):
         opt.make_problem(inp, objective_terms=term, loss=opt.aspect_ratio)
-    with pytest.raises(ValueError, match="derivative_method"):
-        opt.make_problem(
-            inp, objective_terms=term, derivative_method="finite_difference"
-        )
+    finite_difference = opt.make_problem(
+        inp,
+        objective_terms=term,
+        derivative_method="finite_difference",
+        workers=1,
+    )
+    assert finite_difference.metadata["derivative_method"] == "finite_difference"
+    assert "equilibrium re-solves" in finite_difference.metadata[
+        "derivative_description"
+    ]
     with pytest.raises(ValueError, match="non-negative"):
         opt.make_problem(
             inp, objective_terms=[(opt.aspect_ratio, 4.0, -1.0)], max_mode=1

tests/test_problem.py
@@ -243,6 +243,29 @@ def test_jax_fallbacks_and_jacobian_shape_validation():
     )
     with pytest.raises(ValueError, match="must have shape"):
         malformed.residual_and_jac(malformed.x0)
+    separate_malformed = FunctionProblem(
+        [1.0, 2.0], residual=lambda x: x, residual_jac=lambda x: np.ones((2, 1))
+    )
+    with pytest.raises(ValueError, match="one column"):
+        separate_malformed.residual_jac(separate_malformed.x0)
+
+
+def test_separate_residual_does_not_eagerly_compute_jacobian():
+    calls = {"residual": 0, "jacobian": 0}
+
+    def residual(x):
+        calls["residual"] += 1
+        return x - 1.0
+
+    def jacobian(x):
+        calls["jacobian"] += 1
+        return np.eye(x.size)
+
+    problem = FunctionProblem([2.0, 3.0], residual=residual, residual_jac=jacobian)
+    np.testing.assert_array_equal(problem.residual(problem.x0), [1.0, 2.0])
+    assert calls == {"residual": 1, "jacobian": 0}
+    np.testing.assert_array_equal(problem.residual_jac(problem.x0), np.eye(2))
+    assert calls == {"residual": 1, "jacobian": 1}
 
 
 def test_problem_rejects_ambiguous_metadata_shapes():
@@ -337,3 +360,127 @@ def test_direct_jaxopt_and_optax_contracts():
         updates, state = transform.update(grad, state, x)
         x = optax.apply_updates(x, updates)
     assert float(problem.jax_fun(x)) < float(problem.jax_fun(problem.x0))
+
+
+def test_vmec_finite_difference_factory_uses_parallel_provider(monkeypatch):
+    """The VMEC factory composes tuples and differentiates opaque host terms."""
+    from pathlib import Path
+
+    from vmex.core import optimize as opt
+    from vmex.core.input import VmecInput
+
+    inp = VmecInput.from_file(
+        Path(__file__).resolve().parents[1] / "examples/data/input.solovev"
+    )
+
+    def fake_solve(trial, **kwargs):
+        del kwargs
+        return SimpleNamespace(value=np.sum(opt.pack_boundary(trial, 1)))
+
+    monkeypatch.setattr(opt, "solve_equilibrium", fake_solve)
+    problem = opt.VmecProblem.from_tuples(
+        inp,
+        [(lambda equilibrium: equilibrium.value, 0.0, 4.0)],
+        max_mode=1,
+        derivative_method="finite_difference",
+        workers=2,
+    )
+    jacobian = problem.residual_jac(problem.x0)
+    np.testing.assert_allclose(jacobian, np.full((1, problem.x0.size), 2.0))
+    assert problem.metadata["fd_method"] == "3-point"
+
+
+def test_vmec_finite_difference_scalar_and_validation_paths(monkeypatch):
+    """Opaque scalar losses remain open to any gradient-based optimizer."""
+    from pathlib import Path
+
+    from vmex.core import optimize as opt
+    from vmex.core.input import VmecInput
+
+    inp = VmecInput.from_file(
+        Path(__file__).resolve().parents[1] / "examples/data/input.solovev"
+    )
+
+    def fake_solve(trial, **kwargs):
+        del kwargs
+        return SimpleNamespace(value=float(np.sum(opt.pack_boundary(trial, 1))))
+
+    monkeypatch.setattr(opt, "solve_equilibrium", fake_solve)
+    term = lambda equilibrium: np.atleast_1d(equilibrium.value)  # noqa: E731
+    with pytest.raises(ValueError, match="weight_semantics"):
+        opt.make_problem(
+            inp,
+            objective_terms=[(term, 0.0, 1.0)],
+            derivative_method="finite_difference",
+            weight_semantics="bad",
+        )
+    with pytest.raises(ValueError, match="non-negative"):
+        opt.make_problem(
+            inp,
+            objective_terms=[(term, 0.0, -1.0)],
+            derivative_method="finite_difference",
+        )
+    with pytest.raises(FloatingPointError, match="non-finite objective"):
+        opt.make_problem(
+            inp,
+            loss=lambda equilibrium: np.nan,
+            derivative_method="finite_difference",
+        )
+    with pytest.raises(FloatingPointError, match="empty residual"):
+        opt.make_problem(
+            inp,
+            objective_terms=[(lambda equilibrium: np.array([]), 0.0, 1.0)],
+            derivative_method="finite_difference",
+        )
+
+    problem = opt.make_problem(
+        inp,
+        loss=lambda equilibrium: equilibrium.value**2,
+        derivative_method="finite_difference",
+        workers=1,
+        use_ess=False,
+    )
+    value, gradient = problem.value_and_grad(problem.x0)
+    assert np.isfinite(value) and np.all(np.isfinite(gradient))
+
+
+def test_vmec_finite_difference_failed_probe_penalties(monkeypatch):
+    """Failed opaque probes become finite penalties for external optimizers."""
+    from pathlib import Path
+
+    from vmex.core import optimize as opt
+    from vmex.core.input import VmecInput
+
+    inp = VmecInput.from_file(
+        Path(__file__).resolve().parents[1] / "examples/data/input.solovev"
+    )
+    seed = opt.pack_boundary(inp, 1)
+
+    def flaky_solve(trial, **kwargs):
+        del kwargs
+        x = opt.pack_boundary(trial, 1)
+        if not np.array_equal(x, seed):
+            raise RuntimeError("synthetic VMEC failure")
+        return SimpleNamespace(value=float(np.sum(x)))
+
+    monkeypatch.setattr(opt, "solve_equilibrium", flaky_solve)
+    term = lambda equilibrium: np.atleast_1d(equilibrium.value)  # noqa: E731
+    residual_problem = opt.make_problem(
+        inp,
+        objective_terms=[(term, 0.0, 1.0)],
+        derivative_method="finite_difference",
+        workers=1,
+    )
+    trial = seed.copy()
+    trial[0] += 0.1
+    assert np.all(np.isfinite(residual_problem.residual(trial)))
+    assert residual_problem.metadata["holder"]["failed_trials"] == 1
+
+    scalar_problem = opt.make_problem(
+        inp,
+        loss=lambda equilibrium: equilibrium.value,
+        derivative_method="finite_difference",
+        workers=1,
+    )
+    assert np.isfinite(scalar_problem.fun(trial))
+    assert scalar_problem.metadata["holder"]["failed_trials"] == 1